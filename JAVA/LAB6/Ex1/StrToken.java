dd1c4069d8be0286093fd3f38179fc2ecba68bfc
shivanidandir <36872969+shivanidandir@users.noreply.github.com>
2020-01-02 14:44:24 +0530

    Update StrToken.java

--- JAVA/LAB6/Ex1/StrToken.java
+++ JAVA/LAB6/Ex1/StrToken.java
@@ -3,14 +3,15 @@ public class StrToken {
 	public static void main(String args[]){
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter numbers:");
-		String input=sc.next();
+		String input=sc.nextLine();
 		StringTokenizer st=new StringTokenizer(input,",");
 		int sum=0;
 		while(st.hasMoreTokens()){
 			int n=Integer.parseInt(st.nextToken());
 			System.out.println(n);
-			sum+=n;
+			sum= sum+n;
 		}
 		System.out.println("Sum is:"+sum);
+		sc.close();
 	}
 }
